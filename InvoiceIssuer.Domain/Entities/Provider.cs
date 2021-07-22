cb68bdb05fe13b9f4664e931db027d8835562e6f
gabeFrancisco <gabegit2@gmail.com>
2021-07-22 20:23:20 -0300

    Removed collections from Provide and added LogoPath

--- InvoiceIssuer.Domain/Entities/Provider.cs
+++ InvoiceIssuer.Domain/Entities/Provider.cs
@@ -5,8 +5,6 @@ namespace InvoiceIssuer.Domain.Entities
     public class Provider : Company
     {
         public string Password { get; set; }
-        public ICollection<ServiceType> ServiceTypes { get; set; }
-        public ICollection<Taker> Takers { get; set; }
-        public ICollection<Invoice> Invoices { get; set; }
+        public string LogoPath { get; set; }
     }
 }
