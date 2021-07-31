4d7730d0017e34eee0c4cf06fa6ccefc4ff45aae
gabeFrancisco <gabegit2@gmail.com>
2021-07-31 16:28:58 -0300

    Changed bootstrap theme

--- InvoiceIssuer.Domain/Entities/CompanyType.cs
+++ InvoiceIssuer.Domain/Entities/CompanyType.cs
@@ -5,5 +5,6 @@ namespace InvoiceIssuer.Domain.Entities
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal TaxRate { get; set; }
+        
     }
 }
