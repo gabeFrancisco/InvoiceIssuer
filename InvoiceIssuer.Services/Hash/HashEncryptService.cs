7dc203a5bc3986de1e5ca88c706b9f8a287e2718
gabeFrancisco <gabegit2@gmail.com>
2021-07-31 22:55:12 -0300

    Created services layer and added HashEncryptService for passwords

---
+++ InvoiceIssuer.Services/Hash/HashEncryptService.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace InvoiceIssuer.Services.Hash
+{
+    public static class HashEncryptService
+    {
+        public static string GenerateHash(string typedPassword)
+        {
+            SHA512 algorith = SHA512.Create();
+
+            if (string.IsNullOrEmpty(typedPassword))
+                throw new NullReferenceException("You must type your password!");
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(typedPassword);
+            byte[] hash = algorith.ComputeHash(inputBytes);
+
+            StringBuilder builder = new StringBuilder();
+            foreach (var character in hash)
+            {
+                builder.Append(character.ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+    }
+}
